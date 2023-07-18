78b360ba838f27135d8c34990d8c0c26d3abbf68
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-18 14:28:07 -0400

    Added functinality to clear button

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -1,4 +1,5 @@
 containerEl = document.querySelector("#saved-artist-container");
+titleEl = document.querySelector("#title")
 
 $("#home-button").on("click", function(event){
 	document.location.replace("./index.html")
@@ -12,31 +13,43 @@ function displaySavedArtists(){
 
     var savedArtistsArray = localStorage.getItem("savedArtists");
     console.log(savedArtistsArray);
-    var savedArtists = JSON.parse(savedArtistsArray);
-    console.log(savedArtists)
-    console.log(savedArtists[0].artistImage)
-    if(savedArtists){
-        for( i = 0; i < savedArtists.length; i++){
-        newDiv = document.createElement("div");
-        newFig = document.createElement("figure");
-        newImg = document.createElement("img")
-        newLink = document.createElement("a")
-        artistImageUrl = savedArtists[i].artistImage
-        $(newImg).attr("src", artistImageUrl);
-        artistName = savedArtists[i].artistName
-        $(newLink).text(artistName);
-        artistUrl = savedArtists[i].artistUrl
-        $(newLink).attr("href", artistUrl);
-        
-        newFig.appendChild(newImg);
-        newDiv.appendChild(newFig);
-        newDiv.appendChild(newLink);
-        containerEl.appendChild(newDiv);
+    if(!savedArtistsArray){
+        $(titleEl).text("No saved artists, go on a quest and find some!");
     }
-
-
+    else{
+        var savedArtists = JSON.parse(savedArtistsArray);
+        console.log(savedArtists)
+        console.log(savedArtists[0].artistImage)
+        if(savedArtists){
+            for( i = 0; i < savedArtists.length; i++){
+                newDiv = document.createElement("div");
+                newFig = document.createElement("figure");
+                newImg = document.createElement("img")
+                newLink = document.createElement("a")
+                artistImageUrl = savedArtists[i].artistImage
+                $(newImg).attr("src", artistImageUrl);
+                artistName = savedArtists[i].artistName
+                $(newLink).text(artistName);
+                artistUrl = savedArtists[i].artistUrl
+                $(newLink).attr("href", artistUrl);
+                newFig.appendChild(newImg);
+                newDiv.appendChild(newFig);
+                newDiv.appendChild(newLink);
+                containerEl.appendChild(newDiv);
+            }
+            var clearButton = document.createElement("button")
+            $(clearButton).attr("class", "button")
+            $(clearButton).attr("id", "clear-button")
+            $(clearButton).text("CLEAR SAVED ARTISTS")
+            containerEl.appendChild(clearButton);
+        }
     }
-
 }
 
 displaySavedArtists()
+
+$("#clear-button").on("click", function(event){
+    localStorage.clear()
+    location.reload(true);
+});
+
