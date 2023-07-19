4c5974e3bec237f55941eb89ac1743cbb39e9497
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-19 12:08:46 -0400

    Added styles

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,15 @@
 
-
+// takes you to artist page
   $( "#begin-button" ).on( "click", function(event) {
 	document.location.replace('./artist.html');
   });
 
+// copy of button in navbar
   $( "#begin-button2" ).on( "click", function(event) {
     document.location.replace('./artist.html');
     });
 
+// takes you to saved artist page
   $("#saved-button").on("click", function(event){
 	document.location.replace('./saved.html')
   });
